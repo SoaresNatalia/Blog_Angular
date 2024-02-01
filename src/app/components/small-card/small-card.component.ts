818599485bc8d7a72175ab2e3720a7e2b8879152
Natalia Soares <nataliasilvamvn@gmail.com>
2024-01-31 21:28:07 -0300

    Deixando dinâmicas as informações do Blog, usando os mesmos components com info diferentes

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'small-card',
@@ -9,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class SmallCardComponent {
 
+  @Input()
+  fotoSmal :string = '';
+  @Input()
+  tituloSmal :string = '';
 }
